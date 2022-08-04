4ec213e458bb73040354ea82299dea407541de2d
Trygve Edvard Rygg <trygve@ryggnet.com>
2022-08-04 18:23:41 +0200

    Started making main page

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 const express = require('express');
-const https = require('https');
+// const https = require('https');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 const app = express();
 
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/chain.pem')
-}
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/cert.pem'),
+//     ca: fs.readFileSync('/etc/letsencrypt/live/ryggnet.com/chain.pem')
+// }
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    var myres = '<p>You requested: ' + req.url + '</p>\n<h1>My cool server</h1>';
-    res.send(myres);
+// app.get('/', (req, res) => {
+//     var myres = '<p>You requested: ' + req.url + '</p>\n<h1>My cool server</h1>';
+//     res.send(myres);
+// });
+
+// app.all('*', (req, res) => {
+//     res.status(404).send('<h2>The requested resource: ' + req.url + ', was not found</h2>');
+// });
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000....');
 });
 
-app.get('/surdeig', (req, res) => {
-    res.sendFile(path.join(__dirname, 'images', 'sd.jpg'), (err) => {
-        if (err) {
-            console.log('Something went wrong');
-        }
-    });
-});
+// const httpsServer = https.createServer(options, app);
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h2>The requested resource: ' + req.url + ', was not found</h2>');
-});
-
-const httpsServer = https.createServer(options, app);
-
-httpsServer.listen(443, () => {
-        console.log('Listening on port 443');
-});
+// httpsServer.listen(443, () => {
+//         console.log('Listening on port 443');
+// });
